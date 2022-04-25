398b01ff763425b1cb4e50f10efefd7076515ac5
prateek <prateek.mishra@rapyuta-robotics.com>
2022-04-25 17:23:35 +0900

    Initial form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+
+const Form= () => {
+const [userName, setuserName] = useState("");
+const [value, setValue] = useState('unchecked');
+const [name, setName] = useState('');
+const handleChange =(e)=>{
+	if(e.target.value.length===100){
+	window.alert("Maximum characters allowed is only 100")
+	}
+	setuserName(e.target.value);
+}
+
+onChange(e)
+{
+  const re= /^[0-9\b]+$/;
+  if (e.target.value === '' || re.test(e.target.value)){
+    this.setState({value: e.target.value})
+  }
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log('Form submitted, ${name}');
+}
+
+return (
+	<div>
+	<label>
+			Please enter username:
+			</label>
+			<input
+				name="username"
+				value={userName}
+				onChange={ handleChange}
+			/>
+      <label>
+        Please check the box if required
+      </label>
+      <Checkbox 
+      checked={value}
+      onChange={setValue}
+      />
+      <label>
+        Enter the days for availability
+      </label>
+      <input
+        name="Enter days"
+        value={this.state.value}
+        onChange={this.onChange}
+      />
+      <form onSubmit = {handleSubmit}>
+            <input onChange = {(e) => setName(e.target.value)} value = {name}></input>
+            <button type = 'submit'>Click to submit</button>
+        </form>
+    );
+</div>
+)
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+	<div className="App">
+	<h1>Welcome to the sample form created using react JS</h1>
+	<Form/>
+	</div>
+);
 }
+
 
 export default App;
